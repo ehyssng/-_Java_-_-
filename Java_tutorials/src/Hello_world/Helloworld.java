ceb5daeae94a537732a695fbecd622a64297f48b
Hyeonseong Lee <hslee7231@gmail.com>
2022-06-28 22:54:40 +0900

    생활코딩 숫자와 문자 수업

---
+++ Java_tutorials/src/Hello_world/Helloworld.java
@@ -0,0 +1,9 @@
+package Hello_world;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
---
+++ Java_tutorials/src/NumberString/CharString.java
@@ -0,0 +1,25 @@
+package NumberString;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println('생');
+		
+		System.out.println("생활코딩");
+		
+//		System.out.println('생활코딩');
+		
+		System.out.println("생");
+		
+		System.out.println("생활코딩" + "입니다");
+		
+//		System.out.println("egoing said "Welcome programming world"");
+		
+		System.out.println("egoing said \"Welcome programming world\"");
+		
+		System.out.println("egoing said\n \"Welcome programming world\"");
+		
+		System.out.println("egoing said\n \"Welcome\n programming world\"");
+	}
+
+}
