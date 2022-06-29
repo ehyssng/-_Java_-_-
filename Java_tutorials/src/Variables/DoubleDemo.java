4ec4c6daba7f5de6972dd235dea43c002e6836ed
Hyeonseong Lee <hslee7231@gmail.com>
2022-06-29 22:23:35 +0900

    생활코딩 변수 수업

---
+++ Java_tutorials/src/Variables/DoubleDemo.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class DoubleDemo {
+
+	public static void main(String[] args) {
+		double a = 1.1; 
+		System.out.println(a+1.1); // 2.2
+		 
+		a = 2.1; 
+		System.out.println(a+1.1); // 3.2
+		
+//		int a = 1.1; 
+//		System.out.println(a+1.1);	// 오류 발생
+	}
+
+}
---
+++ Java_tutorials/src/Variables/StringDemo.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+//		String first;
+//		first = "coding";
+		
+		String first = "coding"; 
+		System.out.println(first+" "+"everybody");
+		
+		String a, b;
+		a = "coding";
+		b = " everybody";
+		System.out.println(a+b);
+	}
+
+}
