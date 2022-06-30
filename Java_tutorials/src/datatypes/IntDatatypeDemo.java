bf11bce80ae3323979585c76de77d3eab92a4acb
Hyeonseong Lee <hslee7231@gmail.com>
2022-06-30 22:55:39 +0900

    생활코딩 데이터 타입 수업

--- Java_tutorials/src/datatypes/IntDatatypeDemo.java
+++ Java_tutorials/src/datatypes/IntDatatypeDemo.java
@@ -3,8 +3,12 @@ package datatypes;
 public class IntDatatypeDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		byte a;
+		a = 127;
+//		a = 128;	// byte는 -128~127까지밖에 표현이 안된다.
+		
+		short b;
+		b = 128;
 	}
 
 }
